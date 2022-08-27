ac115d5ace90a685d5388dc0b1c61334cd37b9d7
dvil461 <94796532+dvil461@users.noreply.github.com>
2022-08-27 12:18:14 +0400

    Update config.js

--- config.js
+++ config.js
@@ -41,6 +41,14 @@ module.exports = {
 	FORCE_LOGOUT: toBool(process.env.FORCE_LOGOUT),
 	BRAINSHOP: process.env.BRAINSHOP || '159501,6pq8dPiYt7PdqHz3',
 	DIS_BOT: process.env.DISABLE_BOT || 'null',
+        BOT_INFO: 
+                process.env.BOT_INFO || '𝐙𝐄𝐘𝐑𝐎𝐗,𝐌𝐄𝐓𝐑𝐎-𝐁𝐎𝐓',
+        FIND_API: 
+                process.env.FIND_API || 'TOKEN,SECRET',
+        FOOTERMARK:
+		process.env.FOOTERMARK || '©𝐙𝐄𝐘𝐑𝐎𝐗',
+        MENU_MEDIA:
+		process.env.MENU_MEDIA || 'image,https://i.imgur.com/l0t8EEC.jpeg,https://i.imgur.com/S1PcgyU.mp4',
 	ANTILINK_MSG:
 		process.env.ANTILINK_MSG || '_Antilink Detected &mention kicked_',
 	ANTISPAM_MSG:
